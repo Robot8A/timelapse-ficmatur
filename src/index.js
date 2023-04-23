436c82ece21e4b4414308e6fb63cce03f6e6081f
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-04-23 14:26:41 +0200

    Basic leaflet webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import leaflet from 'leaflet'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+let DefaultIcon = L.icon({
+            ...L.Icon.Default.prototype.options,
+            iconUrl: icon,
+            iconRetinaUrl: iconRetina,
+            shadowUrl: iconShadow
+        });
+        L.Marker.prototype.options.icon = DefaultIcon;
+import "./index.css";
 
 function component() {
   const element = document.createElement('div');
-  element.setAttribute("id", "mapid")
+  element.setAttribute("id", "map")
 
   return element;
 }
@@ -10,7 +22,7 @@ function component() {
 document.body.appendChild(component());
 
 
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
