9a3cc84a97c083025fab854a5f85a34468911bb2
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-04-27 12:20:48 +0200

    Add data to geojson, start timeline

--- src/index.js
+++ src/index.js
@@ -5,15 +5,32 @@ const basemap = L.tileLayer.provider("OpenStreetMap.Mapnik").addTo(map);
 layers.addBaseLayer(basemap, "OpenStreetMap");
 layers.addBaseLayer(L.tileLayer.providerESP('PNOA'), "PNOA")
 
-// Add marker
-L.marker([40.42, -3.7]).addTo(map)
-  .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+// Add GeoJSON layer
+$.getJSON("clips.geojson", function (data) {
+  var geojsonLayer = L.geoJSON(data).addTo(map);
+});
+
 
 // Add timeline
+const timeline = L.timeline(geojsonLayer, {
+  onEachFeature(feature, layer) {
+    layer.bindTooltip(feature.properties.name);
+  },
+  style(feature) {
+    return {
+      fill: false,
+      stroke: true,
+      weight: 4,
+      color: feature?.properties?.colour ?? "red",
+    };
+  },
+}).addTo(map);
+
 var timelineControl = L.timelineSliderControl({
+  enableKeyboardControls: true,
   formatOutput: function (date) {
-    return new Date(date).toString();
+    return new Date(date).getFullYear().toString();
   },
 });
 
-timelineControl.addTo(map);
+timelineControl.addTo(map).addTimelines(timeline);
