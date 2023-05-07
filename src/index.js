6d27719afc4f04594221fd609b501bfab4e66617
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-05-07 13:46:14 +0200

    First working prototype of timeline

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
 // Set up map
 var map = L.map('map').setView([40.42, -3.7], 13);
+
+// Add layer control
 const layers = L.control.layers().addTo(map);
+
+// Add base layers (OSM & PNOA)
 const basemap = L.tileLayer.provider("OpenStreetMap.Mapnik").addTo(map);
 layers.addBaseLayer(basemap, "OpenStreetMap");
 layers.addBaseLayer(L.tileLayer.providerESP('PNOA'), "PNOA")
 
 // Add GeoJSON layer
 $.getJSON("clips.geojson", function (data) {
-  var geojsonLayer = L.geoJSON(data).addTo(map);
+  // Add timeline
+  const timeline = L.timeline(data, {
+    onEachFeature(feature, layer) {
+      layer.bindTooltip(feature.properties.name);
+    },
+    style(feature) {
+      return {
+        fill: false,
+        stroke: true,
+        weight: 4,
+        color: feature?.properties?.colour ?? "red",
+      };
+    },
+  }).addTo(map);
+
+  var timelineControl = L.timelineSliderControl({
+    enableKeyboardControls: true,
+    formatOutput: function (date) {
+      return new Date(date).getFullYear().toString();
+    },
+  });
+
+  timelineControl.addTo(map).addTimelines(timeline);
 });
-
-
-// Add timeline
-const timeline = L.timeline(geojsonLayer, {
-  onEachFeature(feature, layer) {
-    layer.bindTooltip(feature.properties.name);
-  },
-  style(feature) {
-    return {
-      fill: false,
-      stroke: true,
-      weight: 4,
-      color: feature?.properties?.colour ?? "red",
-    };
-  },
-}).addTo(map);
-
-var timelineControl = L.timelineSliderControl({
-  enableKeyboardControls: true,
-  formatOutput: function (date) {
-    return new Date(date).getFullYear().toString();
-  },
-});
-
-timelineControl.addTo(map).addTimelines(timeline);
